913fff2a06075d32952380ceff7d2e5358975d48
pinomaker-hoo <inhoo23@naver.com>
2023-08-11 16:58:53 +0900

    feat : Add Logger Format

--- src/util/logger/logger.middleware.ts
+++ src/util/logger/logger.middleware.ts
@@ -8,13 +8,26 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export default class LoggerMiddleware implements NestMiddleware {
   private logger = new Logger();
-  use(request: Request, response: Response, next: NextFunction) {
-    const { method, originalUrl } = request;
 
-    response.on('finish', () => {
-      const { statusCode } = response;
-      this.logger.log(`${method} ${statusCode} - ${originalUrl}`);
+  use(request: Request, response: Response, next: NextFunction) {
+    const { ip, method, originalUrl } = request;
+    const userAgent = request.get('user-agent') || '';
+
+    request.on('close', () => {
+      const contentLength = response.get('content-length');
+
+      this.logger.log(`${method} : ${originalUrl}`);
+      this.logger.log(`${ip} : ${userAgent} - ${contentLength}`);
+
+      if (request.body) {
+        this.logger.log(`Request Body : ${JSON.stringify(request.body)}`);
+      }
+
+      if (request.query) {
+        this.logger.log(`Request Query : ${JSON.stringify(request.query)}`);
+      }
     });
+
     next();
   }
 }
